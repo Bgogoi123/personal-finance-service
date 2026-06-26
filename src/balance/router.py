9e0dbdf112124a25237018a3b92af10c51c54b5f
Bharati Gogoi <bharatigogoi@Bharatis-MacBook-Air.local>
2026-06-26 23:48:53 +0530

    [fix][Dependencies Annotated] - Annotated all dependencies in the router files of each module.

--- src/balance/router.py
+++ src/balance/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
+from typing import Annotated
 from src.utils.db import get_db
 from src.utils.auth.authentication import is_authenticated
 from src.auth.models import UsersModel
@@ -9,23 +9,25 @@ from src.balance.schema import BalanceCreateSchema, BalanceResponseSchema
 from src.balance import controller
 
 balance_routes = APIRouter(prefix="/balance")
+session_dependency = Annotated[AsyncSession, Depends(get_db)]
+user_dependency = Annotated[UsersModel, Depends(is_authenticated)]
 
 # create balance per each user
 @balance_routes.post("/add", response_model=BalanceResponseSchema, status_code=status.HTTP_201_CREATED)
-async def create_balance(body: BalanceCreateSchema, session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated)):
+async def create_balance(body: BalanceCreateSchema, session: session_dependency, user: user_dependency):
   return await controller.create_balance(body, session, user)
 
 # get balance by id for current user
 @balance_routes.get("/{id}", response_model=BalanceResponseSchema, status_code=status.HTTP_200_OK)
-async def get_balance_by_id(id: str, body: BalanceCreateSchema, session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated) ):
+async def get_balance_by_id(id: str, body: BalanceCreateSchema, session: session_dependency, user: user_dependency ):
   return await controller.get_balance_by_id(id, body, session, user)
 
 # update balance by id for current user
 @balance_routes.put("/update/{id}", response_model=BalanceResponseSchema, status_code=status.HTTP_201_CREATED)
-async def update_balance_by_id(id: str, body: BalanceCreateSchema,  session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated) ):
+async def update_balance_by_id(id: str, body: BalanceCreateSchema,  session: session_dependency, user: user_dependency ):
   return await controller.update_balance_by_id(id, body, session, user)
 
 # delete balance by id for current user
 @balance_routes.delete("/delete/{id}")
-async def delete_balance_by_id(id: str, session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated) ):
+async def delete_balance_by_id(id: str, session: session_dependency, user: user_dependency ):
   return await controller.delete_balance_by_id(id, session, user)
--- src/categories/router.py
+++ src/categories/router.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List 
+from typing import Annotated, List
 from src.categories.schema import CategoryResponseSchema, CategoryCreateSchema, CategoryUpdateSchema
 from src.categories import controller
 from src.auth.models import UsersModel
 from src.utils.db import get_db
-
 from src.utils.auth.authentication import is_authenticated
 
 categories_routes = APIRouter(prefix="/categories")
+session_dependency = Annotated[AsyncSession, Depends(get_db)]
+user_dependency = Annotated[UsersModel, Depends(is_authenticated)]
 
 # Create Category
 @categories_routes.post(
@@ -18,27 +19,27 @@ categories_routes = APIRouter(prefix="/categories")
 )
 async def create_category(
   body: CategoryCreateSchema,
-  session: AsyncSession = Depends(get_db),
-  user: UsersModel = Depends(is_authenticated)
+  session: session_dependency,
+  user: user_dependency
 ):
   return await controller.create_category(body, session, user)
 
 # Fetch All Categories
 @categories_routes.get("/", response_model=List[CategoryResponseSchema], status_code=status.HTTP_200_OK)
-async def get_all_categories(session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated)):
+async def get_all_categories(session: session_dependency, user: user_dependency):
   return await controller.get_all_categories(session, user)
 
 # Fetch A Category by ID
 @categories_routes.get("/{id}", response_model=CategoryResponseSchema, status_code=status.HTTP_200_OK)
-async def get_category_by_id(id: str, session: AsyncSession = Depends(get_db), user: UsersModel = Depends(is_authenticated)):
+async def get_category_by_id(id: str, session: session_dependency, user: user_dependency):
   return await controller.get_category_by_id(id, session, user)
 
 # Update A Category by ID
 @categories_routes.put("/update/{id}", response_model=CategoryUpdateSchema, status_code=status.HTTP_201_CREATED)
-async def update_category_by_id(id: str, body: CategoryUpdateSchema, session: AsyncSession = Depends(get_db), user = Depends(is_authenticated)):
+async def update_category_by_id(id: str, body: CategoryUpdateSchema, session: session_dependency, user: user_dependency):
   return await controller.update_category_by_id(id, body, session, user)
 
 # Delete A Category by ID
 @categories_routes.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_category_by_id(id: str, session: AsyncSession = Depends(get_db), user = Depends(is_authenticated)):
+async def delete_category_by_id(id: str, session: session_dependency, user: user_dependency):
   return await controller.delete_category_by_id(id, session, user)
