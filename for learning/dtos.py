9ee2663d8b2fab5dcdc8b7d53afa0bff424bc847
Bharati Gogoi <bharatigogoi@Bharatis-MacBook-Air.local>
2026-07-07 19:57:36 +0530

    [fix][Dir Removal] - Removed 'for learning' directory.

--- for learning/dtos.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-#  DTO --> Data Transfer Object (Data Validation)
-
-class ProductDTO(BaseModel):
-  id: int
-  title: str
-  price: int = 0 #Optional (if no value sent, default 0 value will be set)
-  count: int = 0 #Optional (if no value sent, default 0 value will be set)
