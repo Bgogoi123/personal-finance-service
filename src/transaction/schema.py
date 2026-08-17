3d69441d38129713b0c6ee702010235fef3e97cd
Bharati Gogoi <bharatigogoi@Bharatis-MacBook-Air.local>
2026-08-17 23:49:49 +0530

    [fix][transaction column rename] - renamed 'transaction_title' to 'title' and 'transaction_type' to 'type' in all usages of them in transaction controller and schema.

--- src/transaction/schema.py
+++ src/transaction/schema.py
@@ -3,31 +3,34 @@ from decimal import Decimal
 from datetime import datetime
 from typing import Literal, Optional
 
-TRANSACTION_TYPE  = Literal["income", "expense"]
+TRANSACTION_TYPE = Literal["income", "expense"]
+
 
 class TransactionCreateSchema(BaseModel):
-  transaction_title: str
-  transaction_type: TRANSACTION_TYPE = "expense"
-  amount: Decimal = Decimal("0.00")
-  note: str = None
-  category_id: str
-  payment_option_id: str
-  
+    title: str
+    type: TRANSACTION_TYPE = "expense"
+    amount: Decimal = Decimal("0.00")
+    note: str = None
+    category_id: str
+    payment_option_id: str
+
+
 class TransactionUpdateSchema(BaseModel):
-  transaction_title: Optional[str] = None
-  transaction_type: Optional[TRANSACTION_TYPE] = None
-  amount: Optional[Decimal] = None
-  note: Optional[str] = None
-  category_id: Optional[str] = None
-  payment_option_id: Optional[str] = None
+    title: Optional[str] = None
+    type: Optional[TRANSACTION_TYPE] = None
+    amount: Optional[Decimal] = None
+    note: Optional[str] = None
+    category_id: Optional[str] = None
+    payment_option_id: Optional[str] = None
+
 
 class TransactionResponseSchema(BaseModel):
-  model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True)
 
-  id: str
-  transaction_title: str
-  transaction_type: TRANSACTION_TYPE
-  amount: Decimal
-  note: str | None = None
-  created_at: datetime
-  updated_at: datetime | None = None
+    id: str
+    title: str
+    type: TRANSACTION_TYPE
+    amount: Decimal
+    note: str | None = None
+    created_at: datetime
+    updated_at: datetime | None = None
